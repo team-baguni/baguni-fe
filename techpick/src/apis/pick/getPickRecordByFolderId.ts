507a03c840cda750dc702bd5617153560eb46dd7
dmdgpdi <dmdgpdi@naver.com>
2025-03-13 00:00:10 +0900

    refactor: 더 이상 사용하지 않는 api call 함수 삭제

--- techpick/src/apis/pick/getPickRecordByFolderId.ts
+++
@@ -1,7 +0,0 @@
-import { generatePickRecordData } from '@/utils/generatePickRecordData';
-import { getAllPickListByFolderId } from './getAllPickListByFolderId';
-
-export const getPickRecordByFolderId = async (folderId: number) => {
-  const data = await getAllPickListByFolderId(folderId);
-  return generatePickRecordData(data);
-};
