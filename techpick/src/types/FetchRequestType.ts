9dc1466fdf64ecf30058306f06d6fa580f593939
dmdgpdi <dmdgpdi@naver.com>
2025-03-13 00:06:49 +0900

    refactor: 사용하지 않는 타입 제거

--- techpick/src/types/FetchRequestType.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description zustand에서 api 호출 시에 확인할 수 있는 타입입니다.
- */
-export interface FetchRequestType<T> {
-  isLoading: boolean;
-  data: T | null;
-  isError: boolean;
-  error: string | null;
-}
--- techpick/src/types/GetSharedFolderListRequestType.ts
+++
@@ -1,4 +0,0 @@
-import type { components } from '@/schema';
-
-export type GetSharedFolderListType =
-  components['schemas']['baguni.api.application.sharedFolder.dto.SharedFolderApiResponse$ReadFolderPartial'][];
