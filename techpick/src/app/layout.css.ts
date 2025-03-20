122a2b84f1158493a0198f0aaa1a2d2f7be3ac67
dmdgpdi <dmdgpdi@naver.com>
2025-03-20 20:20:23 +0900

    design: 마우스 selection 색 변경

--- techpick/src/app/layout.css.ts
+++ techpick/src/app/layout.css.ts
@@ -1,5 +1,10 @@
 import { style } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 
 export const preventOverscrollBehaviorY = style({
   overscrollBehaviorY: 'none',
+});
+
+globalStyle('::selection', {
+  backgroundColor: '#e0edfb',
 });
