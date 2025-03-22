4162164cf8c4c48e5bb0ae0e73913ced4785afdd
dmdgpdi <dmdgpdi@naver.com>
2025-03-22 17:17:04 +0900

    feat: 사용하지 않는 drag-select 관련 컴포넌트 제거

--- techpick/src/libs/@drag-select/DragSelectContainer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { type PropsWithChildren, memo } from 'react';
-
-export const DragSelectContainer = memo(function DragSelectContainer({
-  children,
-}: PropsWithChildren) {
-  return <div data-drag-selectable-container>{children}</div>;
-});
